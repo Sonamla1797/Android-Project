790f52ea5a1eedceeb35e2b003fbab49c4faebc1
Sonamla1797 <125848960+Sonamla1797@users.noreply.github.com>
2025-09-19 09:44:35 +1000

    used okhttp client to increase server timeout timeframe

--- app/src/main/java/com/example/myassssmentapplication/RetrofitClient.kt
+++ app/src/main/java/com/example/myassssmentapplication/RetrofitClient.kt
@@ -1,15 +1,24 @@
 package com.example.myassssmentapplication
 
+
 import com.example.myassssmentapplication.api.ApiService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-
+    val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(60, TimeUnit.SECONDS)
+        .writeTimeout(60, TimeUnit.SECONDS)
+        .build()
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://nit3213api.onrender.com/") //
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
     }
 
